78eca2c91dc23eff1d6641a25d921795e3cd758c
Gregory <gende@novacollege.nl>
2022-12-11 01:36:25 +0100

    but wait, there's more

--- Day10.2.php
+++ Day10.2.php
@@ -1,29 +1,44 @@
 <?php
 $lines = explode(PHP_EOL, file_get_contents("Day10.txt"));
 $x = 1;
-$cycles = 0;
-$screen = array_fill(0, 6, array_fill(0, 40, "<div class='empty'></div>"));
+$cycles = 1;
+$screen = array_fill(0, 6, array_fill(1, 40, "<div class='empty'></div>"));
 for ($i = 0; $i < count($lines); $i++) {
     if (str_contains($lines[$i], "noop")) {
         $cycles++;
-        $screen[floor($cycles / 40)][$x] = "<div class='block'></div>";
+        if (abs(($cycles % 40) - $x) <= 1) {
+            $screen[intdiv($cycles, 40)][$cycles] = "<div class='green'></div>";
+        }
     } else {
         $cycles += 1;
-        $screen[floor($cycles / 40)][$x] = "<div class='block'></div>";
+        if (abs(($cycles % 40) - $x) <= 1) {
+            $screen[intdiv($cycles, 40)][$cycles] = "<div class='green'></div>";
+        }
         $cycles += 1;
-        $screen[floor($cycles / 40)][$x] = "<div class='block'></div>";
+        if (abs(($cycles % 40) - $x) <= 1) {
+            $screen[intdiv($cycles, 40)][$cycles] = "<div class='green'></div>";
+        }
         $x += (int)filter_var($lines[$i], FILTER_SANITIZE_NUMBER_INT);
     }
 }
+
+function print_array($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
 foreach ($screen as $row) {
     foreach ($row as $symbol) {
         echo $symbol;
     }
     echo "<br>";
 }
+print_array($screen);
 ?>
 <style>
-    .block {
+    .green {
         background-color: green;
         width: 10px;
         height: 10px;
